d3504d3a0093679c3f57b1afef0c370910364630
arzummammadova <arzuam-af106@code.edu.az>
2025-05-03 21:20:04 +0400

    adding header

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,3 +54,49 @@ const closeBtn = document.querySelector('.close-btn');
 closeBtn.addEventListener('click', () => {
   fullMenu.classList.remove('active');
 });
+
+const header = document.querySelector('.header-container');
+const hero = document.querySelector('#hero');
+let lastScrollY = window.scrollY;
+
+window.addEventListener('scroll', () => {
+  const currentScrollY = window.scrollY;
+  const triggerPoint = hero.offsetHeight * 0.4;
+  
+ 
+  if (currentScrollY > triggerPoint) {
+    header.classList.add('show');
+    header.classList.remove('reset');
+  } 
+  
+  else if (currentScrollY <= triggerPoint) {
+    header.classList.remove('show');
+    header.classList.add('reset');
+  }
+  
+  lastScrollY = currentScrollY;
+});
+
+
+
+const menuButton = document.querySelector('.menu');
+
+const closeButton = document.querySelector('.close-btn');
+
+menuButton.addEventListener('click', () => {
+  fullMenu.classList.add('active');
+  document.body.style.overflow = 'hidden';
+});
+
+closeButton.addEventListener('click', () => {
+  fullMenu.classList.remove('active');
+  document.body.style.overflow = 'auto';
+});
+
+const menuLinks = document.querySelectorAll('.menu-links a');
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    fullMenu.classList.remove('active');
+    document.body.style.overflow = 'auto';
+  });
+});
